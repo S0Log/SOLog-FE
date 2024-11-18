dab28dd267b5d9afe21a4cf0d830b39de22ca441
rasubeen <govl7016@gmail.com>
2024-11-18 20:12:01 +0900

    feat/#2 index page 검색 바 연한 선 해결 및 Chart 컴포넌트 추가

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,9 +6,17 @@ const Search = () => {
       <input
         type="text"
         placeholder="종목을 입력해주세요."
-        className="shadow-lg focus:outline-none w-[55%] py-3 pl-5 rounded-l-3xl placeholder-gray-400"
+        className="w-[55%] py-3 pl-5 rounded-l-3xl placeholder-gray-400 focus:outline-none"
+        style={{
+          boxShadow: "2px 0px 5px rgba(0, 0, 0, 0.1)",
+        }}
       />
-      <button className="h-12 p-2 bg-white rounded-r-3xl shadow-lg flex items-center justify-center">
+      <button
+        className="h-12 p-2 bg-white rounded-r-3xl -ml-[1px] flex items-center justify-center"
+        style={{
+          boxShadow: "2px 0px 5px rgba(0, 0, 0, 0.1)",
+        }}
+      >
         <img src={magnifier} alt="검색 아이콘" className="h-full" />
       </button>
     </div>
