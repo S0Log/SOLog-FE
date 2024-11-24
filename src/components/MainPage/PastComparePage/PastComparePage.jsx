4afbb459e98666bff65170f6fd73f182febc7bb9
here <govl7016@gmail.com>
2024-11-25 01:18:05 +0900

    Revert "pastcomparepage 레이아웃 잡기"

--- src/components/MainPage/PastComparePage/PastComparePage.jsx
+++ src/components/MainPage/PastComparePage/PastComparePage.jsx
@@ -1,16 +1,7 @@
-import React from 'react';
-import PastPage from './PastPage';
-import ComparePage from './ComparePage';
+import React from 'react'
 
 export default function PastComparePage() {
   return (
-    <div className="pastcomparepage flex-grow w-[88vw] h-screen">
-      <div className="m-2 h-[36vh]">
-        <PastPage />
-      </div>
-      <div className="m-2  h-[36vh]">
-        <ComparePage />
-      </div>
-    </div>
-  );
+    <div>PastComparePage</div>
+  )
 }
